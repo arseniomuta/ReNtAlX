2702cb4a87c35d296927a0ce576eb889b23fc502
Arsenio Antonio Muta <arsenioantoniomuta@gmail.com>
2023-05-17 01:28:06 +0100

    some changes done

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,13 +2,13 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/account/infra/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/account/repositories/IUsersRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
-import { CarsImagesRepository } from "@modules/cars/repositories/in-memory/CarsImagesRepository";
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
 
 container.registerSingleton<ICategoriesRepository>(
