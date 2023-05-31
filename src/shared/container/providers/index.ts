3402ce266b3b2fc5a059418a52bf0f977285ce01
Arsenio Antonio Muta <arsenioantoniomuta@gmail.com>
2023-05-31 14:49:47 +0100

    Error create car

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -18,7 +18,12 @@ container.registerSingleton<IMailProvider>(
   EtherealMailProvider
 );
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  S3StorageProvider
+  diskStorage[process.env.disk]
 );
